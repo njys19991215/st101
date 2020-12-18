0afea4e9c7e10cb4d92bf0f339423ca376734905
njys19991215 <73679963+njys19991215@users.noreply.github.com>
2020-12-18 11:39:21 +0800

    Update views.py

--- func1api/views.py
+++ func1api/views.py
@@ -58,8 +58,8 @@ def callback(request):
                    elif mtext == '@快速選單':
                         func.sendQuickreply(event)
               
-                   elif mtext == '@按鈕樣板':
-                        func.sendButton(event)
+                   elif mtext == '珍珠奶茶價格/匯率':
+                        func.sendButton4(event)
     
                    elif mtext == '@確認樣板':
                         func.sendConfirm(event)
